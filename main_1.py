2b5841bebd0ce50aff4251f605973c3b9b0a8cae
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-10-28 11:51:33 +0000

    ESERCIZIO 1

---
+++ main_1.py
@@ -0,0 +1,12 @@
+#ESERCIZIO 1 - CREAZIONE DI UN DATABASE
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase")
