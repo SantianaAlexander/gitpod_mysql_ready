5596a14db138c20dd9dee0b1892ffa30fdf1d81a
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-11-05 12:44:24 +0000

    AGGIUNTA ALLA PREPARAZIONE ALLA VERIFICA

---
+++ prep_Aggiunta.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from flask import Flask, jsonify
+import json
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="Animali"
+)
+mycursor = mydb.cursor()
+app = Flask(__name__)
+
+
+@app.route('/mammiferi', methods=['GET'])
+def get_mammiferi():
+    # Esegui la query per ottenere tutti i dati dalla tabella "Mammiferi"
+    mycursor.execute("SELECT * FROM Mammiferi")
+    rows = mycursor.fetchall()
+
+    # Ottieni i nomi delle colonne
+    columns = [desc[0] for desc in mycursor.description]
+
+    # Converti ogni riga in un dizionario, con le colonne come chiavi
+    mammiferi_list = [dict(zip(columns, row)) for row in rows]
+
+    # Restituisci i dati in formato JSON
+    return jsonify(mammiferi_list)
+
+# Esegui l'app Flask
+if __name__ == '__main__':
+    app.run(debug=True)
